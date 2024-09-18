4662fd40c2196908709d797db7b4dd33bda2ce14
tripli <tripli@zego.im>
2024-09-18 17:04:34 +0800

    Add support for PictureInPicture feature for Android 8.0 and above.

--- android/app/src/main/java/com/example/MainActivity.java
+++ android/app/src/main/java/com/example/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example;
 
+import android.app.PictureInPictureParams;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Rational;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
+  private final String TAG = "MainActivity";
+
+  private PictureInPictureParams pipParams = null;
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -31,5 +40,38 @@ public class MainActivity extends ReactActivity {
         // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
         DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
         );
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    updatePictureInPictureParams();
+  }
+
+  private void updatePictureInPictureParams() {
+    Rational rational = new Rational(9, 16);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+      pipParams = new PictureInPictureParams.Builder()
+              .setAspectRatio(rational)
+              .setAutoEnterEnabled(true)
+              .build();
+      setPictureInPictureParams(pipParams);
+    } else {
+      pipParams = new PictureInPictureParams.Builder()
+              .setAspectRatio(rational)
+              .build();
+    }
+  }
+
+  @Override
+  protected void onUserLeaveHint() {
+    super.onUserLeaveHint();
+    Log.i(TAG, "onUserLeaveHint");
+
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+      enterPictureInPictureMode(pipParams);
+    }
   }
 }
