91295776a05b639b9000edb5525d72c82dfec3b3
tripli <tripli@zego.im>
2024-09-27 10:28:00 +0800

    Display app version in HomePage.

--- src/HomePage.js
+++ src/HomePage.js
@@ -23,7 +23,7 @@ import {
   Header,
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
-
+import DeviceInfo from 'react-native-device-info'
 import { withNavigation } from '@react-navigation/compat';
 
 import ZegoExpressEngine, {ZegoScenario} from 'zego-express-engine-reactnative';
@@ -136,10 +136,6 @@ class Home extends Component {
       <>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView>
-          
-          <ScrollView
-            contentInsetAdjustmentBehavior="automatic"
-            style={styles.scrollView}>
             <Header />
 
             <View style={styles.body}>
@@ -164,7 +160,10 @@ class Home extends Component {
               <View style={styles.descContainer}>
               </View>
             </View>
-          </ScrollView>
+
+            <View style={{ marginTop: 50, alignItems: 'center' }}>
+              <Text style={styles.version}>App Version: {DeviceInfo.getVersion()}.{DeviceInfo.getBuildNumber()}</Text>
+            </View>
         </SafeAreaView>
       </>
     );
@@ -172,13 +171,6 @@ class Home extends Component {
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
   body: {
     backgroundColor: Colors.white,
   },
@@ -195,24 +187,10 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     color: Colors.black,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  version: {
+    fontSize: 16,
+    color: Colors.black,
   },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-  container: StyleSheet.absoluteFillObject
 });
 
 export default withNavigation(Home);
